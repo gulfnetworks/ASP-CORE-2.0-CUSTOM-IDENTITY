6b394720640760c16f27f8e690972d222d8c3ed9
gulfnetworks <gulfnetworks2017@gmail.com>
2018-01-14 12:58:27 +0300

    latest

---
+++ ModifiedDatabaseIdentity/ModifiedDatabaseIdentity/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ModifiedDatabaseIdentity.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public int UserDetailId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string MobileNo { get; set; }
+        public string UserEmail { get; set; }
+        public int JobTitleId { get; set; }
+        public int ManagerId { get; set; }
+        public int OutletId { get; set; }
+        public string ExtraOutletId { get; set; }
+        public int SubscriptionId { get; set; }
+        public int Active { get; set; }
+        public int SettingsId { get; set; }
+    }
+}
---
+++ ModifiedDatabaseIdentity/ModifiedDatabaseIdentity/Models/UserRoleIntPk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModifiedDatabaseIdentity.Models
+{
+    public class UserRoleIntPk : Microsoft.AspNetCore.Identity.IdentityRole<int>
+    {
+        public UserRoleIntPk() : base()
+        {
+
+        }
+        public UserRoleIntPk(string rolename) : base()
+        {
+            Name = rolename;
+        }
+
+    }
+}
